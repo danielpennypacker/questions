2a6b1b43e4e7f91330009ca72710fc3e47f9125c
Michael Balaban <mbalaban1989@gmail.com>
2022-09-01 01:54:53 -0400

    Initial commit

---
+++ form/backend.py
@@ -0,0 +1,77 @@
+import json
+import re
+from flask import Flask
+from flask import request
+from flask import Response
+
+app = Flask(__name__)
+
+
+@app.route("/api/users")
+def create_user():
+    payload = request.get_json()
+    print(payload)
+
+    first_name = payload.get("first_name")
+    last_name = payload.get("last_name")
+    email = payload.get("email")
+    phone = payload.get("phone")
+
+    errors = {}
+    errors["first_name"] = get_first_name_errors(first_name)
+    errors["last_name"] = get_last_name_errors(last_name)
+    errors["email"] = get_email_errors(email)
+    errors["phone"] = get_phone_errors(phone)
+
+    is_error_free = True
+    for field, error in errors.items():
+        if error is not None:
+            is_error_free = False
+
+    if is_error_free:
+        status = 200
+    else:
+        status = 400
+
+    return Response(
+        json.dumps({"errors": errors}),
+        status=status,
+        mimetype="application/json",
+    )
+
+
+def get_first_name_errors(first_name):
+    if type(first_name) is str and len(first_name) > 1:
+        return None
+    else:
+        return "That doesn't look like a first name"
+
+
+def get_last_name_errors(last_name):
+    if type(last_name) is str and len(last_name) > 1:
+        return None
+    else:
+        return "That doesn't look like a last name"
+
+
+def get_email_errors(email):
+    if email == "takenemail@gmail.com":
+        return "That email is taken"
+    is_valid = False
+    if isinstance(email, str):
+        is_valid = bool(
+            re.match(
+                r"^[a-zA-Z0-9_\-\.\+]+" r"@[a-zA-Z0-9_\-\.\+]+" r"\.[a-zA-Z0-9_\.\+]+$",
+                email,
+            )
+        )
+    if is_valid:
+        return None
+    else:
+        return "That's not a valid email address"
+
+
+def get_phone_errors(phone):
+    if type(phone) is int and len(str(phone)) == 10:
+        return None
+    return "That isn't a valid phone number"
